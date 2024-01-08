7ae24f46349d3f2bc278c77f746e199e0a3daa2e
getchan0119 <getchan0119@naver.com>
2024-01-08 16:41:46 +0900

    Products 데이터베이스 연동

--- src/main/java/com/example/final_project/products/controller/ProductsController.java
+++ src/main/java/com/example/final_project/products/controller/ProductsController.java
@@ -1,24 +1,25 @@
 package com.example.final_project.products.controller;
 
 import com.example.final_project.products.entity.Products;
-import com.example.final_project.products.service.ProductsService;
+import com.example.final_project.products.repository.ProductsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class ProductsController {
 
     //RequiredArgsConstructor 를 사용할 땐 final를 꼭 넣도록 하자
-    private final ProductsService productsService;
+    private final ProductsRepository productsRepository;
 
-    @GetMapping("/products/{id}")
-    public String buy(Model model, @PathVariable("id") Integer id) {
-        Products products = this.productsService.getProduct(id);
-        model.addAttribute("products_buy", products);
-        return "products/products_buy";
+    @GetMapping("/products")
+    public String list(Model model) {
+        List<Products> productsList = this.productsRepository.findAll();
+        model.addAttribute("productsList", productsList);
+        return "products";
     }
 }
