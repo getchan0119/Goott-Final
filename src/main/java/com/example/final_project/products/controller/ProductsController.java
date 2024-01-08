5cb598b3203e224f2236075aeef05cc01e45ddbf
getchan0119 <getchan0119@naver.com>
2024-01-08 16:41:46 +0900

    Products 데이터베이스 연동

--- src/main/java/com/example/final_project/products/controller/ProductsController.java
+++ src/main/java/com/example/final_project/products/controller/ProductsController.java
@@ -1,14 +1,25 @@
 package com.example.final_project.products.controller;
 
+import com.example.final_project.products.entity.Products;
+import com.example.final_project.products.repository.ProductsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class ProductsController {
+
+    //RequiredArgsConstructor 를 사용할 땐 final를 꼭 넣도록 하자
+    private final ProductsRepository productsRepository;
+
     @GetMapping("/products")
-    @ResponseBody
-    public String products() {
-       return "products";
+    public String list(Model model) {
+        List<Products> productsList = this.productsRepository.findAll();
+        model.addAttribute("productsList", productsList);
+        return "products";
     }
 }
