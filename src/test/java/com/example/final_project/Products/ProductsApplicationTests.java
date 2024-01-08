df2272e6a4cbf433374928205ca21affc6c651d3
getchan0119 <getchan0119@naver.com>
2024-01-08 16:41:46 +0900

    Products 데이터베이스 연동

--- src/test/java/com/example/final_project/Products/ProductsApplicationTests.java
+++ src/test/java/com/example/final_project/Products/ProductsApplicationTests.java
@@ -7,8 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ProductsApplicationTests {
 
-    @Autowired
-    private ProductsRepository productsRepository;
+    //테스트 코드 작성시 주석 해제
+    //@Autowired
+    //private final ProductsRepository productsRepository;
 
     //To-Do : mysql로 연결중일 때 테스트하는 방법 찾아보기
+    // https://beaniejoy.tistory.com/66 참고해야하지만.. 개인 프로젝트때 활용할 것
+    
+
 }
