dc6fa56b3d6a5e5e265fb9f10477d1906fb10686
getchan0119 <getchan0119@naver.com>
2024-01-08 16:19:29 +0900

    Products 경고 수정

--- src/main/java/com/example/final_project/products/repository/ProductsRepository.java
+++ src/main/java/com/example/final_project/products/repository/ProductsRepository.java
@@ -1,6 +1,8 @@
 package com.example.final_project.products.repository;
 
+import com.example.final_project.products.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsRepository extends JpaRepository {
+public interface ProductsRepository extends JpaRepository<Products, Integer> {
+
 }
