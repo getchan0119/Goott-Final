d273641bc9a5d83fa03e5c13ed9f51068e5bd832
getchan0119 <getchan0119@naver.com>
2024-01-09 15:34:56 +0900

    Products 제품 등록 /products/create 추가 이미지 첨부 가능

--- src/main/java/com/example/final_project/products/entity/Products.java
+++ src/main/java/com/example/final_project/products/entity/Products.java
@@ -1,6 +1,7 @@
 package com.example.final_project.products.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,7 @@ import lombok.Setter;
 public class Products {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id; //인덱스
+    private Long id; //인덱스
 
     private String name; //상품명
 
@@ -18,9 +19,31 @@ public class Products {
     private String explanation; //상품 설명
 
     @Column(columnDefinition = "TEXT")
-    private String image; // 상품 이미지 URL
+    private String image; // 사용자가 업로드한 파일 이름
+
+    @Column(columnDefinition = "TEXT")
+    private String file_name; // 변경된 이미지 이름
+
+    @Column(columnDefinition = "TEXT")
+    private String file_path; // 이미지 저장 위치
 
     private Integer price; // 가격
 
     private Integer Status; // 0 : 판매중, 1 : 판매완료, 추후 옵션 추가 가능성 있음
+
+    @Builder
+    public Products(Long id, String name, String explanation, Integer price, String image, String file_name, String file_path) {
+        this.id = id;
+        this.name = name;
+        this.explanation = explanation;
+        this.price = price;
+        this.image = image;
+        this.file_name = file_name;
+        this.file_path = file_path;
+        this.Status = 0;
+    }
+
+    public Products() {
+
+    }
 }
