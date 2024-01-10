9bf802810013273e541217bf1dc38a3c3779dbef
hyemin03 <bmhero1103@naver.com>
2024-01-11 00:18:50 +0900

    페이지 세션 처리 , 스프링 시큐리티 세션 관리 설정

--- src/main/java/com/example/final_project/login/config/SecurityConfig.java
+++ src/main/java/com/example/final_project/login/config/SecurityConfig.java
@@ -33,6 +33,13 @@ public class SecurityConfig{
                         .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true))
+                .sessionManagement(sessionManagement -> sessionManagement
+                                .maximumSessions(1)     //최대 허용 가능 세션 수 (-1 넣으면 무제한 세션 생성 이므로 비추천.)
+                                .maxSessionsPreventsLogin(true)    // 추가적인 인증 요청이 있을 경우 어떻게 처리할지 정하는 api (true면 현재 사용자 인증 실패, false면 기존 세션 만료)
+                                .expiredUrl("/") // 세션이 만료된 경우 이동 할 페이지 지정
+                )
+
+
         ;
         return http.build();
     }
